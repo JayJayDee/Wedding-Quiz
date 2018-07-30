0273d16d413a352dc4c18c69c823289cd9b0406e
jaydee <jindongp@gmail.com>
2018-07-30 13:55:29 +0900

    added Quiz type definitions. added implementation for QuizPoolModel::getQuizQuestions()

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,12 +5,23 @@ export interface Member {
   phone: string;
   regDate?: Date;
 }
+export enum QuestionType {
+  TEXT = 'TEXT', IMAGE = 'IMAGE'
+}
+export interface QuizQuestion {
+  no: number;
+  content: string;
+  type: QuestionType;
+}
+export interface QuizAnswer {
+  no: number;
+  content: string;
+}
 
 export interface ReqMemberCreate { 
   name: string;
   phone: string;
 }
-
 export interface ReqPickQuiz {
   member_no: number;
 }
--- src/models/quiz-pool-model.ts
+++ src/models/quiz-pool-model.ts
@@ -1,4 +1,40 @@
 
+import * as _ from 'lodash';
+
+import db from '../databases';
+import { ReqPickQuiz, QuizQuestion, QuestionType } from './index';
+
 export const QuizPoolModel = {
   
+  pickQuizFromPool: async function(pick: ReqPickQuiz) {
+
+  },
+
+  getQuizQuestions: async function(quizNo: number): Promise<QuizQuestion[]> {
+    let query: string = 
+    `
+      SELECT 
+        *
+      FROM 
+        wedd_quiz_question 
+      WHERE 
+        quiz_no=?
+      ORDER BY 
+        seq ASC 
+    `;
+    let params: any[] = [quizNo];
+    let rows: any[] = await db.query(query, params);
+
+    let questions: QuizQuestion[] = _.map(rows, (elem: any) => {
+      let type: QuestionType = null;
+      if (elem.type === 'TEXT') type = QuestionType.TEXT;
+      else if (elem.type === 'IMAGE') type = QuestionType.IMAGE;
+      return {
+        no: elem['no'],
+        type: type,
+        content: elem['content']
+      };
+    });
+    return questions;
+  }
 }
