27fc3d2655b96d8c0a739d43e611e01b82ac69ed
jaydee <jindongp@gmail.com>
2018-08-15 23:13:59 +0900

    added implementation for RankModel::getGlobalRanks()

--- src/models/rank-model.ts
+++ src/models/rank-model.ts
@@ -1,10 +1,51 @@
 
+import * as _ from 'lodash';
+
 import db from '../databases';
 import { RankElement } from '.';
 
 export const RankModel = {
 
   getGlobalRanks: async function(): Promise<RankElement[]> {
-    return [];
+    let query = 
+    `
+    SELECT 
+      m.name,
+      m.phone,
+      quiz_enders.*
+    FROM 
+      (SELECT 
+        member_no,
+        SUM(IF(is_win=1, 1, 0)) AS win_count,
+        TIMESTAMPDIFF(SECOND, MIN(played_date), MAX(played_date)) AS play_time
+      FROM 
+        wedd_quiz_play
+      WHERE 
+        is_played=1 
+      GROUP BY 
+        member_no
+      HAVING 
+        COUNT(no) >= 7) AS quiz_enders
+    INNER JOIN 
+      wedd_member m ON m.no=quiz_enders.member_no
+    ORDER BY 
+      win_count DESC,
+      play_time ASC
+    LIMIT 10
+    `;
+    let rows: any[] = await db.query(query);
+    let rank = 1;
+    let elems: RankElement[] = _.map(rows, (row: any) => {
+      let elem: RankElement = {
+        rank: rank,
+        name: row.name,
+        phone: row.phone,
+        win_count: row.win_count,
+        play_time: row.play_time
+      };
+      rank++;
+      return elem;
+    });
+    return elems;
   }
 };
--- src/rest-endpoints/ranking-endpoints.ts
+++ src/rest-endpoints/ranking-endpoints.ts
@@ -2,13 +2,13 @@
 import * as Router from 'koa-router';
 
 import * as SysTypes from '../types/sys-types';
+import { RankModel, RankElement } from '../models';
 
 const router: Router = new Router();
 
 router.get('/ranks', async function(ctx: SysTypes.ExtendedRouterContext, next: () => Promise<any>) {
-  ctx.sendApiSuccess({
-    
-  });
+  let ranks: RankElement[] = await RankModel.getGlobalRanks();
+  ctx.sendApiSuccess(ranks);
 });
 
 router.get('/member/:member_token/rank', async function(ctx: SysTypes.ExtendedRouterContext, next: () => Promise<any>) {
